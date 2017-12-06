6a47c9aa68ac471be567ff1e1a49f19c69fc5ff3
Chris <hextaine@zoho.com>
2017-12-06 10:14:02 -0500

    Added categories and topics

--- html/create_cat.php
+++ html/create_cat.php
@@ -1,17 +1,29 @@
-<?php // create_cat.php
+<?php
+// create_cat.php
+include 'connect.php';
 
-  include 'connect.php';
-  include 'header.php';
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+  // form hasn't been posted yet, so display it
+  echo '<form method="post" action="">
+        Category name: <input type="text" name="cat_name" />
+        Category description: <textarea name="cat_description" /></textarea>
+        <input type="submit" value="Add category" />
+        </form>';
+}
+else {
+  // The form HAS been posted so let's save it
+  $sql = 'INSERT INTO categories(cat_name, cat_description)
+    VALUES(\'\', ' . mysql_real_escape_string($_POST['cat_name']) .
+      ', ' . mysql_real_escape_string($_POST['cat_description']) . ')';
 
-  echo '<tr>';
-    echo '<td class="leftpart">';
-      echo '<h3><a href="category.php?id=">Category name</a></h3> Category description goes here';
-    echo '</td>';
-    echo '<td class="rightpart">';
-      echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
-    echo '<td>';
-  echo '</tr>';
-
-  include 'footer.php';
+  $result = mysql_query($sql);
+  if (!$result) {
+    // invalid / empty result
+    echo 'Error ' . mysql_error();
+  }
+  else {
+    echo 'New category successfully added.';
+  }
+}
 
 ?>
---
+++ html/create_topic.php
@@ -0,0 +1,121 @@
+<?php 
+
+  include 'connect.php';
+  include 'header.php';
+
+  echo '<h2>Crate a topic</h2>';
+  if ($_SESSION['signed_in'] == false) {
+    // the user is not signed in
+    echo 'Sorry, you have to be <a href="/forum/signin.php">signed in</a> to create a topic.';
+  }
+  else {
+    // the user is signed in
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+      // the form hasn't been posted yet, display it
+      // retrieve the categories from the database for use in the dropdown
+      $sql = "SELECT
+                cat_id,
+                cat_name,
+                cat_description
+              FROM
+                categories";
+
+      $result = mysql_query($sql);
+
+      if (!$result) {
+        // the query failed
+        echo 'Error while selecting from database. Please try again later.';
+      }
+      else {
+        if (myswl_num_rows($result) == 0) {
+          // there are no categories, so topic can't be posted
+          if ($_SESSION['user_lecel'] == 1) {
+            echo 'You have not created categories yet.';
+          }
+          else {
+            echo 'Before you can post a topic, you must wait for an admin to create some categories.';
+          }
+        }
+        else {
+          echo '<form method="post" action="">
+                Subject: <input type="text" name="topic_subject" />
+                Category:';
+          echo '<select name="topic_cat">';
+
+          while ($row = mysql_fetch_assoc($result)) {
+            echo '<option value="' . $row['cat_id'] . '">' . $row['cat_name'] . '</option>';
+          }
+
+          echo '</select>';
+
+          echo 'Message: <textarea name="post_content" /></textarea>
+                <input type="submit" value="Create topic" />
+                </form>';
+        }
+      }
+    }
+    else {
+      // start the transaction
+      $query = "BEGIN WORK";
+      $result = mysql_query($query);
+
+      if (!$result) {
+        // The query failed, quit
+        echo 'An error occured while created your topic. Please try again later.';
+      }
+      else {
+        // the form has been posted, so save it
+        // insert the topic into topics table, we'll save the post into the posts table
+        $sql = "INSERT INTO
+                  topics(topic_subject,
+                         topic_date,
+                         topic_cat,
+                         topic_by)
+                  VALUES('" . mysql_real_escape_string($_POST['topic_subject']) . "', 
+                        NOW(),
+                        " . mysql_real_escape_string($_POST['topic_cat']) . ", 
+                        " . $_SESSION['user_id'] . "
+                      )";
+        $result = mysql_query($sql);
+
+        if (!$result) {
+          // something went wrong, display the error
+          echo 'An error occured while inserting your post. Please try again later. ' . mysql_error();
+          
+          $sql = "ROLLBACK;";
+          $result = mysql_query($sql);
+        }
+        else {
+          $topicid = mysql_insert_id();
+
+          $sql = "INSET INTO
+                posts(post_content,
+                      post_date,
+                      post_topic,
+                      post_by)
+                VALUES
+                  ('" . mysql_real_escape_string($_POST['post_content']) . "', 
+                  NOW(),
+                  " . $topicid . ", 
+                  " . $_SESSION['user_id'] . "
+                )";
+          $result = mysql_query($sql);
+
+          if (!$result) {
+            echo 'An error occured while inserting your post. Try again later. ' . mysql_error();
+            $sql = "ROLLBACK;";
+            $result = mysql_query($sql);
+          }
+          else {
+            $sql = "COMMIT;";
+            $result = mysql_query($sql);
+
+            echo 'You have successfully create <a href="topic.php"?id=' . $topicid . '">your new topic</a>.';
+          }
+        }
+      }
+    }
+  }
+
+  include 'footer.php';
+?>
