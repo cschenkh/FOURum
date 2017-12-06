539897b79777b7907b3e20e27181e9e654519d0c
Chris <hextaine@zoho.com>
2017-12-06 10:37:35 -0500

    Finshed adding categories & topics. Added reply functionality.

---
+++ html/reply.php
@@ -0,0 +1,37 @@
+<?php
+
+  include 'connect.php';
+  include 'header.php';
+
+  if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    echo 'This operation is not allowed!';
+  }
+  else {
+    // check if client is signed in
+    if (!$_SESSION['signed_in']) {
+      echo 'You must be signed in to reply!';
+    }
+    else {
+      $sql = "INSERT INTO
+                posts(post_content,
+                      post_date,
+                      post_topic,
+                      post_by)
+              VALUES ('" . $_POST['reply-content'] . "', 
+                      NOW(), 
+                      " . mysql_real_escape_string($_GET['id']) . ", 
+                      " . $_SESSION['user_id'] . ")";
+
+      $result = mysql_query($sql);
+
+      if (!$result) {
+        echo 'Your reply was not able to be submitted! Please try again later.';
+      }
+      else {
+        echo 'Your reply was submitted! See it <a href="topic.php>id=' . htmlentities($_GET['id']) . '">here</a>.';
+      }
+    }
+  }
+
+  include 'footer.php';
+?>
