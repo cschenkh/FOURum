ca608ad9d91a26ce57de64c2fc03c3ee04ccaaeb
Chris <hextaine@zoho.com>
2017-12-07 21:43:20 -0500

    Changed footer.php message to be shorter.

--- html/footer.php
+++ html/footer.php
@@ -2,7 +2,7 @@
     </div><!-- content -->
   </div><!-- wrapper -->
   
-  <div id="footer">Original work by Chris Schenkhuizen for CS 252</div>
+  <div id="footer">By Chris Schenkhuizen for CS 252</div>
   
   </body>
 </html>
